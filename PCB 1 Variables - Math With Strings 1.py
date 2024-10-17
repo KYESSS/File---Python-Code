1f5e9b94dd5601d2a998a4c7e7e6b413ec4e4a25
KYESSS <113056636+KYESSS@users.noreply.github.com>
2024-10-17 19:32:51 +0100

    Add files via upload

---
+++ PCB 1 Variables - Math With Strings 1.py
@@ -0,0 +1,13 @@
+stock_1_RSI = 35
+stock_2_RSI = 60
+stock_3_RSI = 45
+stock_4_RSI = 20
+stock_5_RSI = 40
+stock_6_RSI = 38
+stock_7_RSI = 42
+
+average_RSI = (stock_1_RSI + stock_2_RSI + 
+                stock_3_RSI + stock_4_RSI +
+                stock_5_RSI + stock_6_RSI + stock_7_RSI)/7
+
+print(round(average_RSI))
---
+++ PCB 1 Variables - Math With Strings.py
@@ -0,0 +1,8 @@
+sentence_start = "You have ate "
+sentence_end = " calories"
+
+player1_calories = "640"
+player2_calories = "500"
+
+print(sentence_start + player1_calories + sentence_end)
+print(sentence_start + player2_calories + sentence_end)
